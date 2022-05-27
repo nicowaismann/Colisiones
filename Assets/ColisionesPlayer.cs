9a2afc8a98d41dbd053f1efeb9ffb05fd99f741f
nicowaismann <46961446@est.ort.edu.ar>
2022-05-27 09:10:08 -0300

    Initial commit

---
+++ Assets/ColisionesPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionesPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "DeathWall")
+        {
+            Destroy(gameObject);
+            transform.Translate(-3.76f, 0, -3.99f);
+        }
+        
+    }
+}
